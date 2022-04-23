7c0d66f21a8817526fdc578d3e03b957c02f6f22
tolatolatop <tolatolatop@gmail.com>
2022-04-23 20:53:33 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RunnigSnail',
+    version='0.5.0',
+    packages=['running_snail'],
+    url='',
+    license='',
+    author='tolatolatop',
+    author_email='tolatolatop@gmail.com',
+    description=''
+)
